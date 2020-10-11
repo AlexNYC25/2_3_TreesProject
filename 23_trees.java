e7299b282c430796d9a49fb0b8cb513aa8a6e9d8
Alexis Montes <montes.alexis25@gmail.com>
2020-10-10 23:51:27 -0400

    started initial functions

---
+++ 23_trees.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+
+class treeNode {
+    int key1;
+    int key2;
+    treeNode child1;
+    treeNode child2;
+    treeNode child3;
+    treeNode father;
+
+    treeNode(){
+
+    }
+
+    void printNode(treeNode TNode, BufferedWriter outFile){
+
+    }
+}
